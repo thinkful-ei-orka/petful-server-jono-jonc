085c12fae7b19e5e21d3a032e8f6af2c68206248
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-26 14:53:43 -0700

    now fixed the client origin

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CLIENT_ORIGIN: 'https://petful-client-template.binkeroni.vercel.app',
+  CLIENT_ORIGIN: 'https://petful-client-template-cyan.vercel.app',
   PORT: process.env.PORT || 8080,
 };
