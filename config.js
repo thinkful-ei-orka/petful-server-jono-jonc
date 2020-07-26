99999366d2725eda584ace2224d561904d3a4297
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-26 14:51:08 -0700

    fixed client origin

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  CLIENT_ORIGIN:
-    process.env.CLIENT_ORIGIN ||
-    'https://petful-client-template.binkeroni.vercel.app',
+  CLIENT_ORIGIN: 'https://petful-client-template.binkeroni.vercel.app',
   PORT: process.env.PORT || 8080,
 };
